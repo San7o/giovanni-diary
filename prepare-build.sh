2f9467d71c8b9a7b2e9b9e2c479a3ffb3da85c1f
San7o <santigio2003@gmail.com>
2025-03-23 18:36:30 +0100

    blog: overall style change

--- prepare-build.sh
+++ prepare-build.sh
@@ -4,3 +4,6 @@
 # content. Any preparation should be done here.
 
 cp $HOME/.emacs.d/init.org content/programming/emacs/init.org
+echo -e "\n---\n\n Go Back: file:emacs.org" >> content/programming/emacs/init.org
+
+cp style/simple-giovanni.css public/
