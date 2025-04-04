847e5c267dac639cb65a088498b9885e96025f78
San7o <santigio2003@gmail.com>
2025-04-04 12:42:42 +0200

    project: changed font to Inconsolata

--- prepare-build.sh
+++ prepare-build.sh
@@ -3,12 +3,22 @@
 # This script is automatically run before generating the static
 # content. Any preparation should be done here.
 
+mkdir -p public
+
 cp $HOME/.emacs.d/init.org content/programming/emacs/init.org
 echo -e "\n---\n\n Go Back: [[file:emacs.org][emacs]]" >> content/programming/emacs/init.org
 
-cp style/simple-giovanni.css public/
+# Style
+cp style/simple-giovanni.css public/simple.css
 cp style/favicon.ico public/
 
+# Fonts
+mkdir -p public/fonts
+cp -r fonts/Crimson_Text public/fonts/
+cp -r fonts/EB_Garamond public/fonts/
+cp -r fonts/Inconsolata public/fonts/
+
+# Images
 mkdir -p public/reading/surroundings
 cp -r content/reading/surroundings/images public/reading/surroundings/
 cp -r content/ephemeris/images public/ephemeris
