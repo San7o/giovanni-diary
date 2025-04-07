163bfaa252112e5769f30bb998a671bea5b79db7
San7o <santigio2003@gmail.com>
2025-04-08 00:26:32 +0200

    photography: new: photography category

--- prepare-build.sh
+++ prepare-build.sh
@@ -20,6 +20,7 @@ cp -r fonts/Inconsolata public/fonts/
 mkdir -p public/reading/surroundings
 cp -r content/reading/surroundings/images public/reading/surroundings/
 cp -r content/ephemeris/images public/ephemeris/
+cp -r content/photography/images public/photography
 
 # Dump git commit history
 git log --pretty=format:"- %ad %s" --date=short > content/git-history.txt
